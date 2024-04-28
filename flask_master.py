79a066ef254fd24cbb68b4bf6fd386b2185d07a9
dimaso2 <nikolayevdm@mail.ru>
2024-04-28 18:15:30 +0300

    aaa

---
+++ flask_master.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route('/contacts/')
+def contacts():
+    developer_name = 'Ilon Mask'
+    return render_template('contacts.html', name = developer_name)
+
+
+if __name__ == "__main__":
+    app.run(debug= True)
